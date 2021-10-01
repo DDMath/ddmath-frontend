231de0761699cb56c730e5fdd775bf4496d82a7f
donghyuk <gohyuk528@gmail.com>
2021-10-01 17:54:28 +0900

    Feat: add button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+type ButtonProps = {
+  text: string;
+  className?: string;
+  children?: JSX.Element | JSX.Element[] | string;
+};
+
+function Button({ children, className, text }: ButtonProps) {
+  return (
+    <DefaultButton className={className}>
+      {children}
+      {text}
+    </DefaultButton>
+  );
+}
+
+const DefaultButton = styled.button`
+  cursor: pointer;
+  outline: none;
+`;
+
+export default Button;
