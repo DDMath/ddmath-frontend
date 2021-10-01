fea50c3700ac103423efc321cbed27a63986f0f0
donghyuk <gohyuk528@gmail.com>
2021-10-01 17:51:41 +0900

    Feat: add App.tsx using Router

---
+++ src/components/App.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import styled, { ThemeProvider } from "styled-components";
+
+import { theme } from "./styles/theme";
+import { flexCenter } from "./styles/mixin";
+import { GlobalStyle } from "./styles/globalStyle";
+
+import Home from "./Home";
+
+function App() {
+  return (
+    <DisplayContainer>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Content>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/stages" />
+            <Route path="/stages/:id" />
+            <Redirect to="/" />
+          </Switch>
+        </Content>
+      </ThemeProvider>
+    </DisplayContainer>
+  );
+}
+
+const DisplayContainer = styled.div`
+  ${flexCenter}
+
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Content = styled.div`
+  ${flexCenter}
+
+  width: ${({ theme }) => theme.size.width};
+  height: ${({ theme }) => theme.size.height};
+
+  overflow: hidden;
+  border-radius: 5px;
+`;
+
+export default App;
