b4b54aa2b9388b2858c39df3e2b7bbf41bb75735
donghyuk <gohyuk528@gmail.com>
2021-10-08 01:09:32 +0900

    Fix: modify .env config file

--- src/phaser-game/scenes/Preloader.ts
+++ src/phaser-game/scenes/Preloader.ts
@@ -6,43 +6,43 @@ export default class Preloader extends Phaser.Scene {
   }
 
   preload() {
-    this.load.baseURL = "http://localhost:3000/";
+    this.load.baseURL = process.env.PROD_URL;
 
-    this.load.image("desk1", "background/desk1.png");
-    this.load.image("desk2", "background/desk2.png");
-    this.load.image("desk3", "background/desk3.png");
-    this.load.image("stages", "background/stages.png");
-    this.load.image("stage1", "background/stage1.png");
-    this.load.image("stage2", "background/stage2.png");
-    this.load.image("stage3", "background/stage3.png");
-    this.load.image("board", "game/board.png");
-    this.load.image("card", "card/card.png");
-    this.load.image("kiwi-1", "card/kiwi1.png");
-    this.load.image("kiwi-2", "card/kiwi2.png");
-    this.load.image("kiwi-3", "card/kiwi3.png");
-    this.load.image("orange-1", "card/orange1.png");
-    this.load.image("orange-2", "card/orange2.png");
-    this.load.image("orange-3", "card/orange3.png");
-    this.load.image("strawberry-1", "card/strawberry1.png");
-    this.load.image("strawberry-2", "card/strawberry2.png");
-    this.load.image("strawberry-3", "card/strawberry3.png");
-    this.load.image("music-blue", "card/music-blue.png");
-    this.load.image("music-yellow", "card/music-yellow.png");
-    this.load.image("music-green", "card/music-green.png");
-    this.load.image("trophy-blue", "card/trophy-blue.png");
-    this.load.image("trophy-yellow", "card/trophy-yellow.png");
-    this.load.image("trophy-green", "card/trophy-green.png");
-    this.load.image("check-blue", "card/check-blue.png");
-    this.load.image("check-yellow", "card/check-yellow.png");
-    this.load.image("check-green", "card/check-green.png");
-    this.load.image("star-blue", "card/star-blue.png");
-    this.load.image("star-yellow", "card/star-yellow.png");
-    this.load.image("star-green", "card/star-green.png");
-    this.load.atlas("character-stand", "character/stand.png", "character/stand.json");
-    this.load.atlas("character-run", "character/run.png", "character/run.json");
-    this.load.image("goBack", "game/goBackButton.png");
-    this.load.image("point", "game/point.png");
-    this.load.image("cannon", "game/cannon.png");
+    this.load.image("desk1", "/background/desk1.png");
+    this.load.image("desk2", "/background/desk2.png");
+    this.load.image("desk3", "/background/desk3.png");
+    this.load.image("stages", "/background/stages.png");
+    this.load.image("stage1", "/background/stage1.png");
+    this.load.image("stage2", "/background/stage2.png");
+    this.load.image("stage3", "/background/stage3.png");
+    this.load.image("board", "/game/board.png");
+    this.load.image("card", "/card/card.png");
+    this.load.image("kiwi-1", "/card/kiwi1.png");
+    this.load.image("kiwi-2", "/card/kiwi2.png");
+    this.load.image("kiwi-3", "/card/kiwi3.png");
+    this.load.image("orange-1", "/card/orange1.png");
+    this.load.image("orange-2", "/card/orange2.png");
+    this.load.image("orange-3", "/card/orange3.png");
+    this.load.image("strawberry-1", "/card/strawberry1.png");
+    this.load.image("strawberry-2", "/card/strawberry2.png");
+    this.load.image("strawberry-3", "/card/strawberry3.png");
+    this.load.image("music-blue", "/card/music-blue.png");
+    this.load.image("music-yellow", "/card/music-yellow.png");
+    this.load.image("music-green", "/card/music-green.png");
+    this.load.image("trophy-blue", "/card/trophy-blue.png");
+    this.load.image("trophy-yellow", "/card/trophy-yellow.png");
+    this.load.image("trophy-green", "/card/trophy-green.png");
+    this.load.image("check-blue", "/card/check-blue.png");
+    this.load.image("check-yellow", "/card/check-yellow.png");
+    this.load.image("check-green", "/card/check-green.png");
+    this.load.image("star-blue", "/card/star-blue.png");
+    this.load.image("star-yellow", "/card/star-yellow.png");
+    this.load.image("star-green", "/card/star-green.png");
+    this.load.atlas("character-stand", "/character/stand.png", "/character/stand.json");
+    this.load.atlas("character-run", "/character/run.png", "/character/run.json");
+    this.load.image("goBack", "/game/goBackButton.png");
+    this.load.image("point", "/game/point.png");
+    this.load.image("cannon", "/game/cannon.png");
   }
 
   create() {
