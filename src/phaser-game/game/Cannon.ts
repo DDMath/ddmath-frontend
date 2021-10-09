bdac50740b1f1a0e9a4ba60ac07420810ce510d5
donghyuk <gohyuk528@gmail.com>
2021-10-09 16:24:47 +0900

    Feat: add cannon and modify cannon type to arcade.sprite

--- src/phaser-game/game/Cannon.ts
+++ src/phaser-game/game/Cannon.ts
@@ -1,18 +1,52 @@
 import Phaser from "phaser";
+import Ball from "./Ball";
 import ShotPreview from "./ShotPreview";
 
-export default class Cannon extends Phaser.GameObjects.Container {
-  private shotPreview;
+export default class Cannon extends Phaser.Physics.Arcade.Sprite {
+  private shotPreview!: ShotPreview;
+  private ball!: Ball;
 
   constructor(scene: Phaser.Scene, x: number, y: number, texture: string) {
-    super(scene, x, y);
+    super(scene, x, y, texture);
 
-    const cannon = scene.add.image(0, 0, texture);
-    this.add(cannon);
+    scene.input.on("pointerup", this.handlePointerUp, this);
+    this.scene.physics.add.existing(this);
 
-    this.shotPreview = new ShotPreview(scene);
+    const body = this.body as Phaser.Physics.Arcade.Body;
 
-    scene.input.on("pointerup", () => this.shotPreview.removePreviousPreview());
+    this.setCircle(this.width * 0.5);
+    body.immovable = true;
+    body.setAllowGravity(false);
+  }
+
+  handlePointerUp() {
+    this.loadBall();
+
+    const pointer = this.scene.input.activePointer;
+    const dx = pointer.x - this.x;
+    const dy = pointer.y - this.y;
+
+    const vec = new Phaser.Math.Vector2(dx, dy);
+    vec.normalize();
+
+    this.ball.shoot(vec);
+
+    this.shotPreview.removePreviousPreview();
+  }
+
+  loadBall() {
+    const width = this.scene.scale.width;
+    const height = this.scene.scale.height;
+
+    this.ball = new Ball(this.scene, width / 2, height - 35, "ball");
+  }
+
+  setShotPreview() {
+    this.shotPreview = new ShotPreview(this.scene);
+  }
+
+  get getFiredBall() {
+    return this.ball;
   }
 
   update() {
@@ -32,9 +66,5 @@ export default class Cannon extends Phaser.GameObjects.Container {
 
     this.rotation = rotation + Math.PI / 2;
     this.shotPreview.showPreview(400, 570, vec, 20);
-
-    if (this.shotPreview.shotPreviewsLength() > 1000) {
-      this.shotPreview.removePreviousPreview();
-    }
   }
 }
